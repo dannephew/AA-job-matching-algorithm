3b4c4528c1d78903a736e28c2bf0745b269e7105
MierafM <mierafmulat@gmail.com>
2022-02-16 19:36:26 -0600

    fixed db connection issue

--- server.js
+++ server.js
@@ -3,29 +3,54 @@ const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
+app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
+
 app.listen(3000, function() {
     console.log('listening on 3000')
   });
 
-  /*
-  // connection string is not working, also is it secure to keep connection string here?
 
-const connectionString = "mongodb+srv://<reinforcements>:<reinforcements>@cluster0.2szva.mongodb.net/ReinforcementsData?retryWrites=true&w=majority"
-MongoClient.connect(connectionString, (err, client) => {
-    // ... do something here
-    if (err) 
-        return console.error(err)
+  // is it secure to keep connection string here?
+
+const connectionString = "mongodb+srv://reinforcements:reinforcements@cluster0.2szva.mongodb.net/reinforcements?retryWrites=true&w=majority"
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
     console.log('Connected to Database')
-  })
-  */
-//CRUD handlers
-app.get('/', (req, res) => {
-    // res.send('Hello World')
-    res.sendFile(__dirname + '/index.html')
+    const db = client.db('data')
+    const businessCollection = db.collection('businessCollection')
+
+    //routes (crud handlers) go here
+    app.post('/bizCreateAcc', (req, res) =>{
+
+      businessCollection.insertOne(req.body)
+      .then( result => {
+        console.log("result sent to db", result)
+        res.redirect('/businessInfo')
+      })
+      .catch(error => console.error(error))
+    })
+
+  
+    app.get('/', (req, res) => {
+      // res.send('Hello World')
+
+      // console.log(cursor)/
+      res.sendFile(__dirname + '/index.html')
+    })
+    
+    app.get('/businessInfo', (req, res) => {
+      // res.send('Hello World')
+      //cursor lets you get data from the db - has lots fo methods
+      const cursor = db.collection('businessCollection').find().toArray()
+      .then(results => {
+        console.log(results)
+        res.render('displayBusiness.ejs', {businesses: results})
+      })
+      .catch(error => console.error(error))
+      // console.log(cursor)//cursor lets you get data from the db - has lots fo methods
+    })
   })
 
-app.post('/bizCreateAcc', (req, res) =>{
-    console.log("creating acc...");
-    console.log(req.body);
-})
+
+
