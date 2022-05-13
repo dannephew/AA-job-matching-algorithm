94da15bd7de9ad0e898912102de6492e5e2bb920
Zarif Ceaser <zarifceaser321@gmail.com>
2022-05-13 02:33:29 -0500

    job time slot matching

--- job_match.js
+++ job_match.js
@@ -1,35 +1,48 @@
 // schedules would contain time ranges for business and employee
 // ex. schedules[0] = business time range, schedules[1] = employee time range
-function find_times(schedules) {
-    matches = [];
-    intervals = []
-    for (let time_range of schedules) {
-        //console.log(time_range);
-        for (let time of time_range) {
-            intervals.push(time);
+function getSlots(person) {
+    slots = [];
+    for (let i = 0; i < person.length; i++) {
+        start = person[i][0];
+        end = person[i][1];
+        //console.log([start, end]);
+        inc = 0;
+        while ((start + inc) < end) {
+            slots.push([start+inc, start+100+inc]);
+            inc += 100;
         }
     }
-    //console.log(intervals);
-    intervals = intervals.sort(function(a, b) {return b[0] - a[0];});
-    //console.log(intervals);
-    check = [];
-    while (intervals.length > 0) {
-        time = intervals.pop();
-
-        if (check.length > 0 && check[check.length - 1][1] >= time[0]) {
-            check[check.length - 1][1] = Math.max(time[1], check[check.length - 1][1]);
-        }
-        else {
-            check.push(time);
-        }
-        //console.log(check);
-    }
-
-    return check;
+    return slots;
 }
 
-const business_times = [ [700, 1200], [1400, 1800] ];
+const includeArray = (val, array) => {
+    return val.some(e => Array.isArray(e) && e.every((j, i) => Object.is(array[i], j)));
+  }
+
+function find_times(schedules) {
+    allSlots = [];
+    for (let i = 0; i < 24; i++) {
+        allSlots.push([i*100, (i+1)*100]);
+    }
+
+    freeSlots = [];
+    for (let i = 0; i < allSlots.length; i++) {
+        goodTime = true;
+        for (let j = 0; j < schedules.length; j++) {
+            entityFreeSlots = getSlots(schedules[j]);
+            if (!includeArray(entityFreeSlots, allSlots[i])) {
+                goodTime = false;
+                break;
+            }
+        if (goodTime) {
+            freeSlots.push(allSlots[i]);
+            }
+        }
+    }
+    return freeSlots;
+}
+
+const business_times = [ [700, 1200], [1900, 2000] ];
 const employee_times = [ [600, 1000], [1500, 1900] ];
 const schedules = [business_times, employee_times];
-//console.log(schedules);
 console.log(find_times(schedules));
