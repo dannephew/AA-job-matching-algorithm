66ea4eb001147c208125f2961c17128e136a07c6
John Sanchez <Johnsanchez2024@u.northwestern.edu>
2022-05-18 15:55:23 -0500

    matching by location

---
+++ match_by_location.js
@@ -0,0 +1,10 @@
+// Get the zip codes from the employees
+// Get the zip codes from the employers
+// If there is a match, then append to a 
+//    list of potential employees
+// 
+// Return potential employees (with their info)
+//
+// Perspective from one business
+// See what employees are in the area
+// Go with that
