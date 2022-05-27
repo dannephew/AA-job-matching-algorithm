7d4af98c836dfb23f0406c49d865ad5af4d5b2c6
Zarif Ceaser <zarifceaser321@gmail.com>
2022-05-27 16:52:58 -0500

    candidate history setup

--- controllers/formControl.js
+++ controllers/formControl.js
@@ -6,4 +6,17 @@ function togglePasswordVisibility() {
     } else {
         x.type = "password";
     }
-}
+};
+
+function selectionLimit() {
+    var select = document.querySelector("#workCultureSelect");
+    var total = 0;
+    for (var i = 0; i < select.options.length; i++) {
+        if (select.options[i].selected == true) {
+            total = total + 1
+        }
+    }
+    if (total > 3) {
+        select.options[select.options.selectedIndex].selected=false
+    }
+};
